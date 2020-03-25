e8dced8b86bef071d01426bfecc3b70fa55e87e9
adeola afariogun <kokobilo55@gmail.com>
2020-03-24 17:27:14 -0700

    generate passwork working

---
+++ script.js
@@ -0,0 +1,47 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+var validnumbers="1234567890"
+var validuppercase="ABCDEFGHIJKLMNOPQRSTVWXYZ"
+var validlowercase="abcdefghijklmnopqrstvwxyz"
+var validspecialcharacters="!@#$%^&*"
+var allcharacters=""
+var upassword=""
+// Write password to the #password input
+function writePassword() {
+  var password = getuserinput();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+function getuserinput(){
+  var pswdlength=prompt("length of the password")
+  var yesnumbers=confirm("would you like to have numbers")
+  var yesuppercase=confirm("would you like to have uppercase letters")
+  var yeslowercase=confirm("would you like to have lowercase letters")
+  var yescharacters=confirm("would you like to have special characters")
+  if(yesnumbers){
+    allcharacters+=validnumbers
+  }
+  if(yesuppercase){
+    allcharacters+=validuppercase
+  }
+  if(yeslowercase){
+    allcharacters+=validlowercase
+  }
+  if(yescharacters){
+    allcharacters+=validspecialcharacters
+  }
+  console.log("AcceptedCharacters",allcharacters);
+  for(let i=1;i<=pswdlength;i++){
+    var index = Math.floor(Math.random() * allcharacters.length)
+    console.log(allcharacters[index])
+      upassword+=allcharacters[index]
+  }
+
+ console.log("Password:",upassword)
+ return upassword
+}
